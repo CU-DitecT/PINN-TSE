4ad7ce95723b96fce225503fa4048cc8f0482ebc
ZhaobinMo <30344798+ZhaobinMo@users.noreply.github.com>
2024-02-14 12:32:00 -0500

    Update NGSIM_infer_NN_baseline_arg_2.py

--- NGSIM_infer_NN_baseline_arg_2.py
+++ NGSIM_infer_NN_baseline_arg_2.py
@@ -2,7 +2,7 @@
 
 
 import sys
-sys.path.insert(0, 'Utilities/')
+#sys.path.insert(0, 'Utilities/')
 
 import tensorflow as tf
 import numpy as np
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import scipy.io
 from scipy.interpolate import griddata
-#from plotting import newfig, savefig
+from plotting import newfig, savefig
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.gridspec as gridspec
 import time
